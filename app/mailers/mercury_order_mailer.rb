2e0f2765a7f8c66875a958596cc508445aa67297
Dustin Milam <darklordsushi@yahoo.com>
2016-09-14 11:50:40 -0400

    changed email for testing

--- app/mailers/mercury_order_mailer.rb
+++ app/mailers/mercury_order_mailer.rb
@@ -1,7 +1,7 @@
 class MercuryOrderMailer < ApplicationMailer
 	add_template_helper(ApplicationHelper)
 
-	default to: 'dustin@wittycreative.com'
+	default to: 'darklordsushi@yahoo.com'
 
 	def send_order(body)
 
