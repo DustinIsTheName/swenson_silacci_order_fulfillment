cd9537efbd17c460ee166283060187be430ed834
Dustin Milam <darklordsushi@yahoo.com>
2017-05-10 13:01:58 -0400

    created webhook if it doesn't exist

--- app/models/function.rb
+++ app/models/function.rb
@@ -1,7 +1,24 @@
 class Function < ActiveRecord::Base
 
 	def self.checkForWebhook
-		puts 'check'
+		webhooks = ShopifyAPI::Webhook.all
+		found_webhook = false
+		for webhook in webhooks
+			if webhook.address == "https://swenson-silacci.herokuapp.com/order-creation"
+				found_webhook = true
+			end
+		end
+
+		unless found_webhook
+			new_webhook = ShopifyAPI::Webhook.create({
+				address: "https:\/\/swenson-silacci.herokuapp.com/order-creation",
+				topic: "orders\/create",
+				format: "json"
+			})
+			puts new_webhook
+		else
+			puts "found it"
+		end
 	end
 
 end
